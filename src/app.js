2790c248a4c400fcf68bc3e61bc79e201bc2a8bc
GhaznaviByte <ghaznavizahra77@gmail.com>
2024-02-20 01:28:00 -0800

    Updated contact page content.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 function handleSubmit(event) {
-  event.preventDefault(); // Prevent default form submission
+  event.preventDefault();
 
-  // Get input field values
   const firstName = document.getElementById("firstName").value;
   const lastName = document.getElementById("lastName").value;
 
-  // Display an alert with the user's first and last names
   alert(
     `Thank you, ${firstName} ${lastName}, for reaching out! 🌟 Your message has been received, and we're excited to connect with you. Expect to hear back from us soon. In the meantime, feel free to explore more of what we offer on our website. Have a wonderful day!`
   );
